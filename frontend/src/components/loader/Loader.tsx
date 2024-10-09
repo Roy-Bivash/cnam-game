12c56a50083a85c2f809686b43730b637c447520
biv <biv@mail.com>
2024-10-09 09:39:35 +0200

    feat: add loader

--- frontend/src/components/loader/Loader.tsx
+++ frontend/src/components/loader/Loader.tsx
@@ -2,7 +2,7 @@ import LoaderCSS from "./Loader.module.css";
 
 export function Loader(){
     return(
-        <div className="fixed h-screen w-max flex items-center justify-center">
+        <div className="fixed h-screen w-full flex items-center justify-center bg-slate-500/10">
             <span className={LoaderCSS.loader}></span>
         </div>
     )
--- frontend/src/pages/home/Home.tsx
+++ frontend/src/pages/home/Home.tsx
@@ -6,6 +6,7 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { useState } from "react";
 import { useToast } from "@/hooks/use-toast";
 import { useNavigate } from "react-router-dom";
+import { Loader } from "@/components/loader/Loader";
 
 import { verifyPasswordInput, verifyTextInput } from "@/lib/form";
 import { CustomFetch } from "@/lib/CustomFetch";
@@ -34,6 +35,7 @@ export default function Home(){
         const { correct, message } = verifyTextInput(formData.email || "");
         
         if(!correct){
+            setLoading(false);
             return toast({
                 title: "Error",
                 variant: "destructive",
@@ -48,7 +50,6 @@ export default function Home(){
 
         if(error){
             setLoading(false);
-
             return toast({
                 title: "Error",
                 variant: "destructive",
@@ -75,72 +76,77 @@ export default function Home(){
     }
 
     return (
-        <main className="min-h-screen flex flex-col gap-8 items-center pt-[15vh]">
-            <h2 className="text-3xl">FIP Game</h2>
-            <Tabs defaultValue="connexion" className="w-[400px]">
-                <TabsList className="grid w-full grid-cols-2">
-                    <TabsTrigger value="connexion">Connexion</TabsTrigger>
-                    <TabsTrigger value="inscription">Inscription</TabsTrigger>
-                </TabsList>
-                <TabsContent value="connexion">
-                    <Card>
-                        <form onSubmit={formLogin}>
-                            <CardHeader>
-                                <CardTitle>Connexion</CardTitle>
-                                <CardDescription>
-                                    Connectez vous pour pouvoir jouer au jeu des FIP du CNAM
-                                </CardDescription>
-                            </CardHeader>
-                            <CardContent className="space-y-2">
-                                <div className="space-y-1">
-                                    <Label htmlFor="email">Email</Label>
-                                    <Input onChange={setForm} value={formData.email || ""} name="email" id="email" type="email" placeholder="nom.prenom@exemple.com" />
-                                </div>
-                                <div className="space-y-1">
-                                    <Label htmlFor="password">Mots de passe</Label>
-                                    <Input onChange={setForm} value={formData.password || ""} name="password" id="password" type="password" placeholder="****" />
-                                </div>
-                            </CardContent>
-                            <CardFooter>
-                                <Button type="submit">Se connecter</Button>
-                            </CardFooter>
-                        </form>
-                    </Card>
-                </TabsContent>
-                <TabsContent value="inscription">
-                    <Card>
-                        <form onSubmit={formRegister}>
-                            <CardHeader>
-                                <CardTitle>Inscription</CardTitle>
-                                <CardDescription>
-                                    Inscrivez vous pour pouvoir jouer au jeu des FIP du CNAM
-                                </CardDescription>
-                            </CardHeader>
-                            <CardContent className="space-y-2">
-                                <div className="space-y-1">
-                                    <Label htmlFor="email">Email</Label>
-                                    <Input onChange={setForm} value={formData.email || ""} name="email" type="email" id="email" placeholder="nom.prenom@exemple.com" />
-                                </div>
-                                <div className="space-y-1">
-                                    <Label htmlFor="email">Pseudo</Label>
-                                    <Input onChange={setForm} value={formData.pseudo || ""} name="pseudo" type="text" id="pseudo" placeholder="exemple123" />
-                                </div>
-                                <div className="space-y-1">
-                                    <Label htmlFor="password">Mots de passe</Label>
-                                    <Input onChange={setForm} value={formData.password || ""} name="password" id="password" type="password" placeholder="****" />
-                                </div>
-                                <div className="space-y-1">
-                                    <Label htmlFor="confirm_password">Confirmez votre mots de passe</Label>
-                                    <Input onChange={setForm} value={formData.confirm_password || ""} name="confirm_password" id="confirm_password" type="password" placeholder="****" />
-                                </div>
-                            </CardContent>
-                            <CardFooter>
-                                <Button>S'inscrire</Button>
-                            </CardFooter>
-                        </form>
-                    </Card>
-                </TabsContent>
-            </Tabs>
-        </main>
+        <>
+            {loading && (
+                <Loader />
+            )}
+            <main className="min-h-screen flex flex-col gap-8 items-center pt-[15vh]">
+                <h2 className="text-3xl">FIP Game</h2>
+                <Tabs defaultValue="connexion" className="w-[400px]">
+                    <TabsList className="grid w-full grid-cols-2">
+                        <TabsTrigger value="connexion">Connexion</TabsTrigger>
+                        <TabsTrigger value="inscription">Inscription</TabsTrigger>
+                    </TabsList>
+                    <TabsContent value="connexion">
+                        <Card>
+                            <form onSubmit={formLogin}>
+                                <CardHeader>
+                                    <CardTitle>Connexion</CardTitle>
+                                    <CardDescription>
+                                        Connectez vous pour pouvoir jouer au jeu des FIP du CNAM
+                                    </CardDescription>
+                                </CardHeader>
+                                <CardContent className="space-y-2">
+                                    <div className="space-y-1">
+                                        <Label htmlFor="email">Email</Label>
+                                        <Input onChange={setForm} value={formData.email || ""} name="email" id="email" type="email" placeholder="nom.prenom@exemple.com" />
+                                    </div>
+                                    <div className="space-y-1">
+                                        <Label htmlFor="password">Mots de passe</Label>
+                                        <Input onChange={setForm} value={formData.password || ""} name="password" id="password" type="password" placeholder="****" />
+                                    </div>
+                                </CardContent>
+                                <CardFooter>
+                                    <Button type="submit">Se connecter</Button>
+                                </CardFooter>
+                            </form>
+                        </Card>
+                    </TabsContent>
+                    <TabsContent value="inscription">
+                        <Card>
+                            <form onSubmit={formRegister}>
+                                <CardHeader>
+                                    <CardTitle>Inscription</CardTitle>
+                                    <CardDescription>
+                                        Inscrivez vous pour pouvoir jouer au jeu des FIP du CNAM
+                                    </CardDescription>
+                                </CardHeader>
+                                <CardContent className="space-y-2">
+                                    <div className="space-y-1">
+                                        <Label htmlFor="email">Email</Label>
+                                        <Input onChange={setForm} value={formData.email || ""} name="email" type="email" id="email" placeholder="nom.prenom@exemple.com" />
+                                    </div>
+                                    <div className="space-y-1">
+                                        <Label htmlFor="email">Pseudo</Label>
+                                        <Input onChange={setForm} value={formData.pseudo || ""} name="pseudo" type="text" id="pseudo" placeholder="exemple123" />
+                                    </div>
+                                    <div className="space-y-1">
+                                        <Label htmlFor="password">Mots de passe</Label>
+                                        <Input onChange={setForm} value={formData.password || ""} name="password" id="password" type="password" placeholder="****" />
+                                    </div>
+                                    <div className="space-y-1">
+                                        <Label htmlFor="confirm_password">Confirmez votre mots de passe</Label>
+                                        <Input onChange={setForm} value={formData.confirm_password || ""} name="confirm_password" id="confirm_password" type="password" placeholder="****" />
+                                    </div>
+                                </CardContent>
+                                <CardFooter>
+                                    <Button>S'inscrire</Button>
+                                </CardFooter>
+                            </form>
+                        </Card>
+                    </TabsContent>
+                </Tabs>
+            </main>
+        </>
     )
 }
