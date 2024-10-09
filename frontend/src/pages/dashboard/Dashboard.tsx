dbd7b07f021a7b367a3dc1c1dd0d94f8d17c7e84
biv <biv@mail.com>
2024-10-09 12:08:44 +0200

    feat: navbar design

--- frontend/src/pages/dashboard/Dashboard.tsx
+++ frontend/src/pages/dashboard/Dashboard.tsx
@@ -1,3 +1,19 @@
+import { Button } from "@/components/ui/button"
+import { Separator } from "@/components/ui/separator"
+import {
+    AlertDialog,
+    AlertDialogAction,
+    AlertDialogCancel,
+    AlertDialogContent,
+    AlertDialogDescription,
+    AlertDialogFooter,
+    AlertDialogHeader,
+    AlertDialogTitle,
+    AlertDialogTrigger,
+  } from "@/components/ui/alert-dialog"
+import { Input } from "@/components/ui/input";
+  
+
 import { getMe } from "@/lib/current";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
@@ -18,8 +34,46 @@ export default function Dashboard(){
     },[])
 
     return(
-        <main className="min-h-screen">
-            <h1 className="text-3xl">Dashboard</h1>
-        </main>
+        <>
+            <nav className="px-6 py-3 border-b">
+                <ul className="flex gap-4 items-center justify-end">
+                    <li className="text-xs">
+                        Online: 23
+                    </li>
+                    <Separator className="h-5" orientation="vertical" />
+                    <li>
+                        <AlertDialog>
+                            <AlertDialogTrigger>
+                                <Button variant="secondary">Jouer avec un amie</Button>
+                            </AlertDialogTrigger>
+                            <AlertDialogContent>
+                                <AlertDialogHeader>
+                                    <AlertDialogTitle>Vous avez un code ?</AlertDialogTitle>
+                                    <AlertDialogDescription>
+                                        <Input name="code" id="code" type="text" placeholder="Votre code" />
+                                        
+                                    </AlertDialogDescription>
+                                </AlertDialogHeader>
+                                <AlertDialogFooter>
+                                    <AlertDialogCancel>Cancel</AlertDialogCancel>
+                                    <AlertDialogAction>Continue</AlertDialogAction>
+                                </AlertDialogFooter>
+                            </AlertDialogContent>
+                        </AlertDialog>
+                    </li>
+                    <li>
+                        <Button variant="default">Jouer</Button>
+                    </li>
+                </ul>
+            </nav>
+            <main className="">
+
+                <div id="deck_list">
+                    
+                </div>
+
+
+            </main>
+        </>
     )
 }
