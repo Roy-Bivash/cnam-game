232e7f802c42280b4287cd459032761bf519e084
biv <biv@mail.com>
2024-10-30 10:12:21 +0100

    clean

--- frontend_next/src/app/dashboard/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Page(){
-    return(
-        <main className="mx-[50px] lg:mx-[100px] mt-[25px] lg:mt-[45px]">
-            <h1 className="text-3xl">Dashboard</h1>
-        </main>   
-    )
-}
--- frontend_next/src/app/test/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function Page() {
-    return <h1>Hello from the test page</h1>
-}
